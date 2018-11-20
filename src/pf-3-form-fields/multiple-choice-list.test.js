e44ac2199902b835cf7b457ffae448bae5964ae1
Martin Maroši <marvusm.mmi@gmail.com>
2018-11-20 16:06:55 +0100

    Array form component tests.

---
+++ src/pf-3-form-fields/multiple-choice-list.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import { Form } from 'react-final-form';
+import ComponentType, { configureContext } from '../renderer-context';
+
+describe('Pf3 <MultipleChoiceList />', () => {
+  it('should render correctly', () => {
+    expect(true)
+  });
+});
